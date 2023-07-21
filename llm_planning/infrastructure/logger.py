6405b32086a3f36987d1b768f8ae0036822b5f97
yessense <akorchemnyj@gmail.com>
2023-07-21 12:50:30 +0300

    Add logger

--- llm_planning/infrastructure/logger.py
+++ llm_planning/infrastructure/logger.py
@@ -1,30 +1,72 @@
 import logging
+from typing import Optional, Dict
 import wandb
 import os
 from abc import ABC, abstractmethod
+import sys
 
 
 class PlanningLogger(ABC):
     _name: str
-    _project_name: str
+
+    @property
+    def name(self):
+        return self._name
 
     def __init__(self,
                  log_dir: str,
-                 project_name: str,
                  log_filename: str):
         self._log_dir = log_dir
         self._log_filename = os.path.join(self._log_dir, log_filename)
-        self._logger = logging.getLogger(self._name)
 
     @abstractmethod
-    def log_text(self, text: str):
+    def info(self, text: str):
         raise NotImplementedError
 
 
 class WandbLogger(PlanningLogger):
-    def __init__(self, log_dir: str, project_name: str, log_filename: str):
-        
+    _LOG_FILENAME: str = 'run.log'
+    _name: str = 'WandbLogger'
 
-    super().__init__(log_dir, project_name, log_filename)
-    def log_text(self, text: str):
-        pass
+    def __init__(self,
+                 log_filename: Optional[str],
+                 log_dir: str = '.',
+                 run_name: str = 'run_name',
+                 project_name: str = 'wandb_test_llm_planning',
+                 log_to_stdout: bool = False):
+        super().__init__(log_dir, self._LOG_FILENAME)
+        logging.basicConfig(filename=self._LOG_FILENAME if self._log_filename is None else self._log_filename,
+                            filemode='a')
+        self._logger = logging.getLogger(self._name)
+        self._logger.setLevel(logging.DEBUG)
+
+        if log_to_stdout:
+            self._logger.addHandler(logging.StreamHandler(sys.stdout))
+
+        self.info(f"Initialized logger for project: '{project_name}'")
+        self.info(f"Run name: '{run_name}'")
+        self.info(f"Initializing wandb...")
+        self._wandb_logger = wandb.init(project=project_name,
+                                        name=run_name,
+                                        dir=self._log_dir)
+
+    def info(self, msg: str):
+        self._logger.info(msg)
+
+    def wandb_log(self, msg: Dict) -> None:
+        self._wandb_logger.log
+
+
+if __name__ == "__main__":
+    project_name = 'wandb_test_llm_planning'
+    log_filename = 'test_logger.log'
+    log_to_stdout = True
+    run_name = 'run_name'
+    log_dir = '.'
+    logger = WandbLogger(log_filename=log_filename,
+                         project_name=project_name,
+                         log_to_stdout=log_to_stdout,
+                         run_name=run_name,
+                         log_dir=log_dir)
+
+    logger.info('hello')
