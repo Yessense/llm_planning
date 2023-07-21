4c59396fef2cd19372cea4e3d74839219ac5d43e
yessense <akorchemnyj@gmail.com>
2023-07-21 12:51:16 +0300

    Add llama_7b

--- llm_planning/models/base_model.py
+++ llm_planning/models/base_model.py
@@ -1,11 +1,49 @@
-from abc import ABC
-from typing import Optional
+from abc import ABC, abstractmethod
+from typing import Any, Optional
 
 from llm_planning.infrastructure.logger import PlanningLogger
 
 
+    
 class BaseLLMModel(ABC):
-    """Base class for LLM models"""
-
+    """ Base class for LLM models"""
     _name: str
-    _logger: Optional[PlanningLogger]
+    _logger: PlanningLogger
+
+    @property
+    def name(self):
+        return self._name
+
+    def __init__(self,
+                 name: str,
+                 logger: PlanningLogger,
+                 **kwargs) -> None:
+        self._name = name
+        self._logger = logger
+
+        self._logger.info(f"Loading {name} model...")
+        self._load()
+        self._logger.info(f'Model: {name}\n')
+
+    def _prepare_for_generation(self) -> None:
+        """Define pipeline, etc."""
+        return None
+
+    # def _prepare_for_scoring(self) -> None:
+    #     """Define scoring for saycan mode"""
+    #     pass
+
+    @abstractmethod
+    def generate(self, prompt: str, **kwargs) -> str:
+        """ Generate text"""
+        pass
+
+    # @abstractmethod
+    # def score_text(self, **kwargs) -> Any:
+    #     """ Score text for saycan approach """
+    #     pass
+
+    @abstractmethod
+    def _load(self) -> None:
+        """Load model, tokenizer, etc"""
+        pass
---
+++ llm_planning/models/llama_7b.py
@@ -0,0 +1,61 @@
+import torch
+from llm_planning.infrastructure.logger import WandbLogger
+from llm_planning.models.base_model import BaseLLMModel
+from transformers import AutoModelForCausalLM, LlamaTokenizer
+from transformers import pipeline
+
+from typing import Any
+import pprint
+
+
+class LLAMA7B(BaseLLMModel):
+    MODEL_NAME = "decapoda-research/llama-7b-hf"
+
+    def __init__(self,
+                 logger: WandbLogger,
+                 device: int = 2,
+                 name: str = 'llama_7b') -> None:
+        self.max_new_tokens = 45
+        self.device = device
+        super().__init__(name=name, logger=logger)
+        # self.logger.info(f"Device map: \n{pprint.pformat(device_map)}")
+
+    # def score_text(self, **kwargs) -> Any:
+    #     pass
+    def _load(self) -> None:
+        self.model = AutoModelForCausalLM.from_pretrained(
+            "decapoda-research/llama-7b-hf",
+            torch_dtype=torch.float16,
+            # load_in_8bit=True,
+            device_map={'': self.device},
+        )
+        self.model.eval()
+
+        self._logger.info(
+            f"Model device map: \n{pprint.pformat(self.model.hf_device_map)}")
+
+        self.tokenizer = LlamaTokenizer.from_pretrained(self.MODEL_NAME)
+        self._prepare_for_generation()
+
+
+    def _prepare_for_generation(self) -> None:
+        self.generation_pipeline = pipeline("text-generation",
+                                            model=self.model,
+                                            tokenizer=self.tokenizer)
+
+    def generate(self, prompt):
+        # generate plan
+        output = self.generation_pipeline(prompt,
+                                          do_sample=False,
+                                          return_full_text=False,
+                                          max_new_tokens=self.max_new_tokens)
+
+        return output[0]['generated_text']
+
+if __name__ == "__main__":
+    wandb_logger = WandbLogger(log_filename=None)
+    model = LLAMA7B(name='llama_7b', logger=wandb_logger)
+
+    answer = model.generate("questin")
+
+    print(answer)
