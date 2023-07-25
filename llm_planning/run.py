25c59fbff4b97b50f52b9fa942229bb755c5cf0f
yessense <akorchemnyj@gmail.com>
2023-07-25 16:18:34 +0300

    Added configs and autoregressive

--- llm_planning/run.py
+++ llm_planning/run.py
@@ -4,23 +4,18 @@ import hydra
 from dataclasses import dataclass
 
 from llm_planning.datasets.strl_robotics import STRLDataset
-from llm_planning.gen_methods.full_plan_generation import FullPlanGeneration
+from llm_planning.gen_methods.autoregressive import AutoregressivePlanGeneration
+from llm_planning.gen_methods.full import FullPlanGeneration
+from llm_planning.infrastructure.config import LLMPlanningConfig
 from llm_planning.infrastructure.logger import WandbLogger
 from torch.utils.data import Subset, random_split
 from llm_planning.metrics.lcs import LCSMetrics
 from llm_planning.models.llama_7b import LLAMA7B
 
 from hydra.core.config_store import ConfigStore
+from hydra.utils import instantiate
 from llm_planning.processors.strl_processor import STRLProcessor
 
-
-@dataclass
-class LLMPlanningConfig:
-    log_filename: str = 'run.log'
-    logging_dir: str = "${hydra:run.dir}/"
-    seed: int = 0
-    device: int = 0
-    dataset_path: str = "data/plans.json"
 
 
 cs = ConfigStore.instance()
@@ -30,48 +25,61 @@ cs.store(name="planner", node=LLMPlanningConfig)
 @hydra.main(version_base=None, config_path=".", config_name="planner")
 def run(cfg=LLMPlanningConfig) -> None:
     generator = torch.Generator()
-    generator.manual_seed(cfg.seed)
+    generator.manual_seed(cfg.experiment.seed)
 
-    logger = WandbLogger(log_filename=cfg.log_filename,
-                         log_dir=cfg.logging_dir,
-                         log_to_stdout=False)
+    logger: WandbLogger = instantiate(cfg.logger)
+    # logger = WandbLogger(log_filename=cfg.log_filename,
+    #                      log_dir=cfg.logging_dir,
+    #                      log_to_stdout=False)
 
-    dataset = STRLDataset(cfg.dataset_path)
+    # dataset = STRLDataset(cfg.dataset_path)
+    dataset = instantiate(cfg.dataset, logger=logger)
+
     train_split, example_split = random_split(
         dataset, [0.8, 0.2], generator=generator)
 
-    processor = STRLProcessor(logger=logger)
+    processor = instantiate(cfg.processor, logger=logger)
+    # processor = STRLProcessor(logger=logger)
     processor.build_system_prompt(example_split)
 
-    model = LLAMA7B(logger=logger,
-                    device=cfg.device,
-                    name='llama_7b')
+    model = instantiate(cfg.model, logger=logger)
+    # model = LLAMA7B(logger=logger,
+    #                 device=cfg.device,
+    #                 name='llama_7b')
 
-    gen_method = FullPlanGeneration(logger=logger,
-                                    processor=processor,
-                                    model=model)
-    lcs_metrics = LCSMetrics(logger=logger,
-                             processor=processor)
+    # gen_method = FullPlanGeneration(logger=logger,
+    #                                 processor=processor,
+    #                                 model=model)
+    
+    gen_method = instantiate(cfg.gen_method, 
+                             logger=logger,
+                             processor=processor,
+                             model=model)
+    # gen_method= AutoregressivePlanGeneration(logger=logger,
+    #                                          processor=processor,
+    #                                          model=model,
+    #                                          saved_steps_path=cfg.saved_steps_path)
+    metrics = instantiate(cfg.metrics,
+                          logger=logger,
+                          processor=processor)
+    # lcs_metrics = LCSMetrics(logger=logger,
+    #                          processor=processor)
 
     for i, gt_task in enumerate(train_split):
 
         gt_task.text = processor._steps_to_text(gt_task.steps)
         predicted_task = gen_method.predict(gt_task)
-        metrics = lcs_metrics.update(predicted_task=predicted_task,
+        curr_metrics = metrics.update(predicted_task=predicted_task,
                            target_task=gt_task)
         logger.info(f"Goal:           {gt_task.goal}")
         logger.info(f"GT plan:        {processor._steps_to_text(gt_task.steps)}")
         logger.info(f"Predicted plan: {predicted_task.text}")
-        logger.info(f"Metrics:        {metrics}\n\n")
-    # gt_task = train_split[-1]
-    # gt_task.text = processor._steps_to_text(gt_task.steps)
-    # predicted_task = gen_method.predict(gt_task)
-    # metrics = lcs_metrics.update(predicted_task=predicted_task,
-    #                     target_task=gt_task)
+        logger.info(f"Metrics:        {curr_metrics}\n\n")
 
-    total_metrics = lcs_metrics.calculate_metrics()
+    total_metrics = metrics.calculate_metrics()
     # total_metrics = {key: f'{value:0.3f}' for key, value in total_metrics.items()}
     logger.info(f"Total_metrics:  {total_metrics}")
+    logger.wandb_log(total_metrics)
     logger.info(f"Done.")
 
 
